3bad11f10ddcac5da5d59d5e9a81e6f84b257f04
litao <1031924189@qq.com>
2018-11-22 17:58:12 +0800

    compelete canvas

--- src/reducers/counter.ts
+++ src/reducers/counter.ts
@@ -2,7 +2,7 @@ import { ADD, MINUS } from '../constants/counter'
 
 const INITIAL_STATE = {
   num: 0,
-  colors: [1,2,3,4,5,6,7]
+  colors: [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 }
 
 export default function counter (state = INITIAL_STATE, action) {
